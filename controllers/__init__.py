247a480cd4c5aecb271e07a40a3ebaab903d6a31
Rebekah <heal.rebekah@gmail.com>
2021-02-01 21:54:53 +1000

    added home page

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,8 +1,10 @@
 # register controllers that will be used
 from controllers.shelters_controller import shelters
 from controllers.animals_controller import animals
+from controllers.home_controller import home
 
 registerable_controllers = [
     shelters,
-    animals
+    animals,
+    home
 ]
