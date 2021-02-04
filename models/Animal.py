5e4ec180e7e587fcbd59233dee136a00036c9b38
Rebekah <heal.rebekah@gmail.com>
2021-02-04 13:50:52 +1000

    added foreign key to animal

--- models/Animal.py
+++ models/Animal.py
@@ -4,8 +4,8 @@ class Animal(db.Model):
     __tablename__ = "animals"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    kind = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    kind = db.Column(db.String, nullable=False)
     breed = db.Column(db.String)
     age = db.Column(db.String)
-    shelter_id = db.Column(db.Integer)
+    shelter_id = db.Column(db.Integer, db.ForeignKey("shelters.id"), nullable=False)
